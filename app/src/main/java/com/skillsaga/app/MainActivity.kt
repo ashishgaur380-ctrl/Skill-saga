f6aad8f52eb2f337f4d42d8adb8e197a003513f5
ashishgaur381-stack <ashishgaur381@gmail.com>
2026-09-16 16:49:18 +0000

    Save latest Skill Saga version

--- app/src/main/java/com/skillsaga/app/MainActivity.kt
+++ app/src/main/java/com/skillsaga/app/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.skillsaga.app
 
 import android.annotation.SuppressLint
-import android.app.Activity
 import android.os.Bundle
+import android.webkit.ConsoleMessage
 import android.webkit.WebChromeClient
+import android.webkit.WebResourceError
+import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : Activity() {
+class MainActivity : AppCompatActivity() {
+
     private lateinit var webView: WebView
 
     @SuppressLint("SetJavaScriptEnabled")
@@ -15,18 +20,52 @@ class MainActivity : Activity() {
         super.onCreate(savedInstanceState)
 
         webView = WebView(this)
+
         webView.settings.javaScriptEnabled = true
         webView.settings.domStorageEnabled = true
         webView.settings.allowFileAccess = true
-        webView.webViewClient = WebViewClient()
-        webView.webChromeClient = WebChromeClient()
-        webView.loadUrl("file:///android_asset/index.html")
+        webView.settings.allowContentAccess = true
+        webView.settings.databaseEnabled = true
+        webView.settings.javaScriptCanOpenWindowsAutomatically = true
+        webView.settings.setSupportMultipleWindows(false)
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun onReceivedError(
+                view: WebView?,
+                request: WebResourceRequest?,
+                error: WebResourceError?
+            ) {
+                if (request?.isForMainFrame == true) {
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Skill Saga loading error: ${error?.description}",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+        }
+
+        webView.webChromeClient = object : WebChromeClient() {
+            override fun onConsoleMessage(message: ConsoleMessage): Boolean {
+                android.util.Log.e(
+                    "SkillSagaWeb",
+                    "${message.message()} -- line ${message.lineNumber()}"
+                )
+                return true
+            }
+        }
 
         setContentView(webView)
+
+        webView.loadUrl("file:///android_asset/index.html")
     }
 
     @Deprecated("Deprecated in Android API 33")
     override fun onBackPressed() {
-        if (webView.canGoBack()) webView.goBack() else super.onBackPressed()
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
     }
 }
